e12adacdf2d4e1b53075347face7400ffdec2619
BrayanBJ27 <xavi_braian@hotmail.com>
2024-12-10 18:57:24 -0500

    feat: :sparkles: Create Context

--- react-app/src/context/AuthContext.tsx
+++ react-app/src/context/AuthContext.tsx
@@ -7,17 +7,33 @@ enum StateTest {
     'authenticated'
 }
 
-export const AuthContext = createContext({})
+interface AuthState {
+    statetValue: StateTest
+    loginEmailWithPassword: (email:string, password:string) => void;
+    logoutEmailWithPassword: () => void
+}
+
+export const AuthContext = createContext({} as AuthState)
 
 export const useAuthContext = () => useContext(AuthContext)
 
 export const AuthProvider = ({children}:PropsWithChildren) => {
     
-    const [statet, setStatet] = useState(StateTest.checking)
+    const [state, setstate] = useState(StateTest.checking)
+
+    const loginEmailWithPassword = (email:string, password:string) => {
+        setstate(StateTest.login)
+    }
+
+    const logoutEmailWithPassword = () => {
+        setstate(StateTest.closed)
+    }
     
     return (
         <AuthContext.Provider value={{
-            statetValue: statet
+            statetValue: state,
+            loginEmailWithPassword,
+            logoutEmailWithPassword
         }}>
             {children}
         </AuthContext.Provider>
